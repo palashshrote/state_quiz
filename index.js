5598c4b0c42c8efce39d2a59db6aa56c220a6ae4
Palash Shrote <palash.shrote.58@gmail.com>
2025-05-05 19:42:03 +0530

    prototype ready

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const port = 3005;
+const app = express();
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "root123",
+  port: 5432,
+});
+db.connect();
+//middleware and bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
+
+var currentQuestion;
+var userScore;
+let quiz = [
+  { region: "India", capital: "New Delhi" },
+  { region: "Russia", capital: "Moscow" },
+  { region: "Nepal", capital: "Kathmandu" },
+];
+db.query("SELECT * FROM stateutcapital", (err, res) => {
+  if (err) {
+    console.log("db error: " + err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+app.get("/", (req, res) => {
+  nextQuestion();
+  userScore = 0;
+  res.render("index.ejs", { question: currentQuestion, score: userScore });
+
+  // res.send("hello");
+});
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  var userAnswer = req.body.answer;
+  var ansCorrect = false;
+  if (userAnswer.toLowerCase() === currentQuestion.capital.toLowerCase()) {
+    console.log("correct");
+    userScore++;
+    ansCorrect = true;
+  }
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    score: userScore,
+    isCorrect: ansCorrect,
+  });
+});
+
+function nextQuestion() {
+  currentQuestion = quiz[Math.floor(Math.random() * quiz.length)];
+}
